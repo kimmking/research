f888f93b1f3c5d5619e53be18069a45f5775aa3e
kimmking <kimmking@163.com>
2023-04-04 15:15:15 +0800

    verify exec-maven-plugin

---
+++ qedis/src/main/java/cn/kimmking/research/qedis/EchoLua.java
@@ -0,0 +1,21 @@
+package cn.kimmking.research.qedis;
+
+import java.io.File;
+
+/**
+ * Description for this class.
+ *
+ * @Author : kimmking(kimmking@apache.org)
+ * @create 2023/4/4 15:00
+ */
+public class EchoLua {
+    public static void main(String[] args) {
+        File file = new File("src/main/resources/lua");
+        System.out.println("lua dir => " + file.getAbsolutePath());
+        if(file.isDirectory()) {
+            for (File lua : file.listFiles()) {
+                System.out.println("lua script => " + lua.getName());
+            }
+        }
+    }
+}
